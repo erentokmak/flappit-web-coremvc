489820135845dbba373693a9f64d1a4ffb002f49
erkankisi <89303485+erkankisi@users.noreply.github.com>
2022-03-03 15:43:24 +0300

    .

---
+++ carfully-web-coremvc/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace carfully_web_coremvc.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+

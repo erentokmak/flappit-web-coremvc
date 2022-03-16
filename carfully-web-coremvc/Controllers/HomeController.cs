ef148dba05b2d2cfe21cf47cd3b42511a9c0089a
erkankisi <89303485+erkankisi@users.noreply.github.com>
2022-03-16 13:05:46 +0300

    altyapı

--- carfully-web-coremvc/Controllers/HomeController.cs
+++ carfully-web-coremvc/Controllers/HomeController.cs
@@ -1,21 +1,30 @@
-﻿using carfully_web_coremvc.Models;
+﻿using carfully_web_coremvc.Helper;
+using carfully_web_coremvc.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace carfully_web_coremvc.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            DbHelper dbHelper = new DbHelper();
+            var x = dbHelper.QueryToTableOnlyConnectionString(_configuration.GetConnectionString("DefaultConnection"), "select * from AspNetUsers");
+
             return View();
         }
 
---
+++ carfully-web-coremvc/Helper/DbHelper.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace carfully_web_coremvc.Helper
+{
+    public class DbHelper
+    {
+        public DataTable OnlyConnectionString(string Con, string queryText)
+        {
+            using (DbDataAdapter adapter = new SqlDataAdapter(queryText, Con))
+            {
+                SqlConnection con = new SqlConnection(Con);
+                SqlCommand cmd = new SqlCommand(queryText, con);
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public DataTableCollection QueryToTableOnlyConnectionString(string Con, string queryText)
+        {
+            SqlConnection nwindConn = new SqlConnection(Con);
+            SqlCommand selectCMD = new SqlCommand(queryText, nwindConn);
+            selectCMD.CommandTimeout = 3000;
+            SqlDataAdapter customerDA = new SqlDataAdapter();
+            customerDA.SelectCommand = selectCMD;
+            nwindConn.Open();
+            DataSet customerDS = new DataSet();
+            customerDA.Fill(customerDS, queryText.Trim());
+            nwindConn.Close();
+
+            return customerDS.Tables;
+        }
+    }
+}
